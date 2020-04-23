f21eaff6171a6d464274d943aca1fdb56c83823e
AilurusWeb <contact@jeanmarie-huchet.fr>
2020-04-23 13:20:06 +0200

    Ajout class DiceConntroller

--- src/dice/dice.service.ts
+++ src/dice/dice.service.ts
@@ -5,12 +5,29 @@ import express, { Request, Response } from "express";
 import { Dice } from "./dice.interface";
 
 /**
+ * Class Dice 
+ */
+export class DiceController {
+  private _dice: Dice = { side: 0, value: 0 };
+
+  public constructor (side: number) {
+    if(typeof side === "number" && side > 0) {
+      this._dice = {
+        side: side,
+        value: Math.floor(Math.random() * side) + 1
+      }
+    }
+  }
+
+  public get() {
+    return this._dice;
+  }
+}
+
+/**
  * Service Methods
  */
 
 export const create = async (side: number): Promise<Dice> => {
-  return {
-    side: side,
-    value: Math.floor(Math.random() * side) + 1
-  }
+  return new DiceController(side).get();
 };
